5b44b5a8fb5cff324b6b4649fdf0aa34ee804b0a
NickToony <nicktoony@gmail.com>
2014-02-21 02:13:45 +0000

    Improved error handling of requesting. Commented all steps of the code, as it can be complex at first. Also, tidied up code and structure.

--- src/com/team5/fragment/SocialFragment.java
+++ src/com/team5/fragment/SocialFragment.java
@@ -11,34 +11,28 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
 import com.team5.pat.R;
-import com.team5.pat.Session;
-import com.team5.pat.R.layout;
 import com.team5.social.Request;
 import com.team5.social.Response;
 import com.team5.social.NetworkInterface;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.app.Activity;
 import android.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class SocialFragment extends Fragment implements NetworkInterface {		
-	private static final int STATE_IDLE = 0;
-	private static final int STATE_REQUESTING = 1;
-	private static final int STATE_FATAL = 2;
+public class SocialFragment extends Fragment implements NetworkInterface {	
+	// WORK IN PROGRESS - Nick
+	// private static final int STATE_IDLE = 0;
+	// private static final int STATE_REQUESTING = 1;
+	// private static final int STATE_FATAL = 2;
 	
-	private final int LAYOUT_REQUESTING = R.layout.activity_social;
+	// private final int LAYOUT_REQUESTING = R.layout.activity_social;
 	
-	private int myState = STATE_IDLE;
-	private Request myRequest;
+	// private int myState = STATE_IDLE;
+	// private Request myRequest; // If you're dealing with multiple requests, you can store its object to keep track
 	private View myView;
 	
 	@Override
@@ -46,54 +40,65 @@ public class SocialFragment extends Fragment implements NetworkInterface {
 		super.onCreate(savedInstanceState);
 		myView = inflater.inflate(R.layout.activity_social, container, false);
 		
-		// Begin step events
-		myRequest = new Request(this, "http://193.35.58.219/PAT/android/login.php", "email=Nick&pass=Pass");
-		myState = STATE_REQUESTING;
+		// Make a request
+		new Request(this, "http://193.35.58.219/PAT/android/login.php", "email=Nick&pass=Pass");
 		
 		return myView;
 	}
 
 	@Override
 	public void eventNetworkResponse(Request request, Response response) {
-		String output = "Didn't do anything";
+		// Default toast text
+		String theOutput = "Didn't do anything";
+		// if request was successful
 		if (response.isSuccess())	{
-			output = "Successful.";
+			theOutput = "Successful.";
 		}	else	{
-			output = "Not successful: " + response.getMessage();
+			// output using the error message provided
+			theOutput = "Not successful: " + response.getMessage();
 		}
 		
-		Toast.makeText(getActivity().getApplicationContext(), output, 
+		// Make the toast with the connection message
+		Toast.makeText(getActivity().getApplicationContext(), theOutput, 
 				   Toast.LENGTH_LONG).show();
 		
-		myState = STATE_FATAL;
-		
+		// Find the TextView
 		TextView myText = (TextView) myView.findViewById(R.id.output);
 		
+		// Don't continue if connection failed
 		if (!response.isSuccess())	{
 			return;
 		}
 		
-		TransformerFactory tf = TransformerFactory.newInstance();
-		Transformer transformer;
-		try {
-			transformer = tf.newTransformer();
-		} catch (TransformerConfigurationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return;
-		}
-		transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-		StringWriter writer = new StringWriter();
-		try {
-			transformer.transform(new DOMSource(response.getDocument()), new StreamResult(writer));
-		} catch (TransformerException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return;
-		}
-		String dd = writer.getBuffer().toString().replaceAll("\n|\r", "");
+		// Make a transformer factory
+		TransformerFactory theTransformerFactory = TransformerFactory.newInstance();
+		Transformer theTransformer;
+		// We're transforming the XML into a string, so need a string writer
+		StringWriter theWriter = new StringWriter();
+
 		
-		myText.setText(dd);
+		try {
+			// Try to create a transformer object
+			theTransformer = theTransformerFactory.newTransformer();
+		} catch (TransformerConfigurationException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		// Tell the transformer we're dealing with XML
+		theTransformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+		try {
+			// Attempt to convert XML into String
+			theTransformer.transform(new DOMSource(response.getDocument()), new StreamResult(theWriter));
+		} catch (TransformerException e) {
+			e.printStackTrace();
+			return;
+		}
+		// Now make a string from the writer
+		theOutput = theWriter.getBuffer().toString();
+		
+		// Put that string into the text view
+		myText.setText(theOutput);
 	}
 
 }
--- src/com/team5/social/Request.java
+++ src/com/team5/social/Request.java
@@ -30,87 +30,125 @@ public class Request {
 		this.myUrlParameters = myUrlParameters;
 		this.myTarget = myTarget;
 		
+		// Start the ASYNC task
 		new RequestTask().execute();
 	}
-	
-	
 	
 	private class RequestTask  extends AsyncTask<Void, Void, Response>	{
 		@Override
 		protected Response doInBackground(Void... params) {
+			// Log that we're starting a request
 			Log.i("Request", "Request.doInBackground() — making a request ");
 			
-			Response myResponse = new Response();
+			// Make the response object
+			Response theResponse = new Response();
+			// And other objects we'll need
+			URL theUrl;
+			HttpURLConnection theConnection;
+			DataOutputStream theOutputStream;
+			Document theDoc;
 			
 			// Get a new document builder factory
 			DocumentBuilderFactory documentBuilder = DocumentBuilderFactory.newInstance();
 			
-			// Have the factory create a new document
+			// Have the factory create a new document builder
 			DocumentBuilder myDocBuilder;
 			try {
 				myDocBuilder = documentBuilder.newDocumentBuilder();
 			} catch (ParserConfigurationException e) {
-				myResponse.set(false, "Failed to configure parser.", null);
+				// Failure
+				theResponse.set(false, "Failed to configure parser.", null);
 				return null;
 			}
 			
-			URL url;
+			// Handle url generation
 			try {
-				url = new URL(myUrl);
+				// Attempt to create the URL object
+				theUrl = new URL(myUrl);
 			} catch (MalformedURLException e2) {
-				myResponse.set(false, "Malformed URL.", null);
-				return myResponse;
+				// Failure
+				theResponse.set(false, "Malformed URL.", null);
+				return theResponse;
 			}
-			HttpURLConnection connection;
-			try {
-				connection = (HttpURLConnection) url.openConnection();
-			} catch (IOException e2) {
-				myResponse.set(false, "Failed to setup connection.", null);
-				return myResponse;
-			}
-			connection.setDoOutput(true);
-			connection.setDoInput(true);
-			connection.setInstanceFollowRedirects(false);
-			try {
-				connection.setRequestMethod("POST");
-			} catch (ProtocolException e1) {
-				myResponse.set(false, "Failed to initiate protocols.", null);
-				return myResponse;
-			}
-			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded"); 
-			connection.setRequestProperty("charset", "utf-8"); 
-			connection.setRequestProperty("Content-Length", "" + Integer.toString(myUrlParameters.getBytes().length));
-			connection.setUseCaches (false);
 			
-			DataOutputStream wr;
+			
+			// Handle HTTP connection
 			try {
-				wr = new DataOutputStream(connection.getOutputStream());
-				wr.writeBytes(myUrlParameters);
-				wr.flush();
-				wr.close();
+				// Attempt to connect to the web server
+				theConnection = (HttpURLConnection) theUrl.openConnection();
+			} catch (IOException e2) {
+				// No connection
+				theResponse.set(false, "Failed to setup connection.", null);
+				return theResponse;
+			}
+			// We want to output data to the server (POST)
+			theConnection.setDoOutput(true);
+			// We want to take data back from the server (the response)
+			theConnection.setDoInput(true);
+			// We refuse any redirects
+			theConnection.setInstanceFollowRedirects(false);
+			
+			
+			try {
+				// Attempt to set form data mode to POST, rather than GET
+					// NOTE: Doing this triggers it to connect to the server (hence no .connect() further up)
+				theConnection.setRequestMethod("POST");
+			} catch (ProtocolException e1) {
+				// Couldn't setup protocols, usually occurs when connection failed.
+				theResponse.set(false, "Failed to initiate protocols. Server not responding?", null);
+				theConnection.disconnect();
+				return theResponse;
+			}
+			
+			// Output HTTP header properties
+			theConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+			// the POST data's format
+			theConnection.setRequestProperty("charset", "utf-8"); 
+			// the POST data's size
+			theConnection.setRequestProperty("Content-Length", "" + Integer.toString(myUrlParameters.getBytes().length));
+			
+			// NO caching
+			theConnection.setUseCaches (false);
+			
+			// We need to write the POST form data
+			try {
+				// Create a DataOutputStream to help us write to the connections output stream to the server
+				theOutputStream = new DataOutputStream(theConnection.getOutputStream());
+				// Write the form data
+				theOutputStream.writeBytes(myUrlParameters);
+				// Push it to the output stream
+				theOutputStream.flush();
+				// No longer need the DataOutputStream
+				theOutputStream.close();
+				// Debug
 				Log.i("Request", "Request.doInBackground() — wrote parameters: " + myUrlParameters);
 			} catch (IOException e1) {
-				myResponse.set(false, "Failing to attach parameters.", null);
-				e1.printStackTrace();
-				return myResponse;
+				// Encoding issues?
+				theResponse.set(false, "Failing to attach parameters.", null);
+				theConnection.disconnect();
+				return theResponse;
 			}
 			
 			// Attempt to get the XML and parse it
-			Document doc;
 			try {
-				doc = myDocBuilder.parse(connection.getInputStream());
-				myResponse.set(true, "Response fetched successfully.", doc);
-				return myResponse;
+				// We take the data from the stream from the server, and try to parse it
+				theDoc = myDocBuilder.parse(theConnection.getInputStream());
+				theResponse.set(true, "Response fetched successfully.", theDoc);
+				theConnection.disconnect();
+				return theResponse;
 			} catch (SAXException e) {
-				myResponse.set(false, "XML parsing failure.", null);
-				return myResponse;
+				theResponse.set(false, "XML parsing failure.", null);
+				theConnection.disconnect();
+				return theResponse;
 			} catch (IOException e) {
-				myResponse.set(false, "Connection failure.", null);
-				return myResponse;
+				theResponse.set(false, "Connection failure.", null);
+				theConnection.disconnect();
+				return theResponse;
 			}
 		}
 		
 		protected void onPostExecute(Response response)	{
+			// Trigger the event on the object which made the request
 			myTarget.eventNetworkResponse(myObject, response);
 		}
 	}
