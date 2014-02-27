fc110076d23d6198b2dd1e47fba7ac5b89e528f5
NickToony <nicktoony@gmail.com>
2014-02-27 04:59:07 +0000

    Implemented a list of topics.

--- src/com/team5/social/ListFragment.java
+++ src/com/team5/social/ListFragment.java
@@ -1,124 +1,134 @@
 package com.team5.social;
 
-import com.team5.pat.HomeActivity;
-import com.team5.pat.R;
+import java.util.ArrayList;
+import java.util.List;
 
-import android.app.ActionBar;
-import android.app.ActionBar.Tab;
-import android.app.ActionBar.TabListener;
 import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+import android.content.Context;
 import android.os.Bundle;
-import android.support.v13.app.FragmentStatePagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class ListFragment extends Fragment implements SocialFragmentInterface, TabListener, OnPageChangeListener  {
+import com.team5.pat.HomeActivity;
+import com.team5.pat.R;
+
+public class ListFragment extends Fragment implements SocialFragmentInterface  {
 	private View myView;
 	private HomeActivity myActivity;
-	private SocialFragment myParent;
-	private ViewPager myPager;
-	private ActionBar myActionBar;
+	private MainFragment myParent;
+	
+	private ListView listView;
+	private ListAdapter listAdapter;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)	{
 		super.onCreate(savedInstanceState);
-		myView = inflater.inflate(R.layout.social_fragment_login, container, false);
+		myView = inflater.inflate(R.layout.social_fragment_list, container, false);
 		myActivity = (HomeActivity) getActivity();
-		myActivity.setTitle("Discussion - Login");
+		myActivity.setTitle("Discussion");
 		
-		myActionBar = myActivity.getActionBar();
-		myActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		myActionBar.setDisplayHomeAsUpEnabled(true);
-		myActionBar.setHomeButtonEnabled(true);
-		myActionBar.removeAllTabs();
-
-		ActionBar.Tab tabTop = myActionBar.newTab().setText("Top").setTabListener(this);
-		ActionBar.Tab tabNew = myActionBar.newTab().setText("New").setTabListener(this);
-		ActionBar.Tab tabMine = myActionBar.newTab().setText("Mine").setTabListener(this);
-
-		myActionBar.addTab(tabTop, 0);
-		myActionBar.addTab(tabNew, 1);
-		myActionBar.addTab(tabMine, 2);
+		// Get the list view
+		listView = (ListView) myView.findViewById(R.id.social_fragment_list_listView);
+		// Create a custom adapter
+		listAdapter = new ListAdapter(myActivity, R.layout.social_fragment_list_row);
 		
-		myPager = (ViewPager) myView.findViewById(R.id.social_fragment_login_pager);
-		myPager.setAdapter(new PagerAdapter(getFragmentManager()));
-		myPager.setOnPageChangeListener(this);
+		// Insert test data..
+		listAdapter.addItem(new ListItem("A new topic", "Hello there, I am a subtitle.", 1));
+		listAdapter.addItem(new ListItem("Another topic", "Hello there, I am another subtitle.", 2));
+		listAdapter.addItem(new ListItem("Proof", "..that the listview works.", 3));
+		listAdapter.addItem(new ListItem("Desperate plea", "A story of anxiety. Lovely!", 4));
+		
+		// Finally, assign the custom adapter to the list
+		listView.setAdapter(listAdapter);
+		
+		
 		
 		return myView;
 	}
 
 	@Override
 	public Fragment setParentFragment(Fragment frag) {
-		this.myParent = (SocialFragment) frag;
+		this.myParent = (MainFragment) frag;
 		return this;
 	}
-
-	@Override
-	public void onPageScrollStateChanged(int arg0) {
-		// TODO Auto-generated method stub
+	
+	
+	/*
+	 * ListItem
+	 * 
+	 * Contains data of each item in the list
+	 */
+	private class ListItem {
+		public String title;
+		public String description;
+		public int id;
 		
-	}
-
-	@Override
-	public void onPageScrolled(int arg0, float arg1, int arg2) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onPageSelected(int position) {
-		myActionBar.setSelectedNavigationItem(position);
-	}
-
-	@Override
-	public void onTabReselected(Tab arg0, FragmentTransaction arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onTabSelected(Tab tab, FragmentTransaction frag) {
-		if (myPager != null)
-			myPager.setCurrentItem(tab.getPosition());
-	}
-
-	@Override
-	public void onTabUnselected(Tab arg0, FragmentTransaction arg1) {
-		// TODO Auto-generated method stub
-		
+		public ListItem(String title, String description, int id)	{
+			this.title = title;
+			this.description = description;
+			this.id = id;
+		}
 	}
 	
-	private class PagerAdapter extends FragmentStatePagerAdapter {
-
-		public PagerAdapter(FragmentManager fm) {
-			super(fm);
+	
+	/*
+	 * ListAdapter
+	 * 
+	 * Adapter for the topics list
+	 */
+	private class ListAdapter extends ArrayAdapter<ListItem> {
+		private List<ListItem> items = new ArrayList<ListItem>();
+		private Context context;
+		private int resource;
+		
+		public ListAdapter(Context context, int resource) {
+			super(context, resource);
+			this.context = context;
+			this.resource = resource;
 		}
-
+		
 		@Override
-		public Fragment getItem(int position) {
-			Fragment fragment = null;
-			switch (position) {
-			case 0:
-				fragment = new SigninFragment();
-				break;
-			case 1:
-				fragment = new SignupFragment();
-				break;
-			case 2:
-				fragment = new SigninFragment();
-				break;
+		public View getView(int position, View convertView, ViewGroup parent) {
+			myView = convertView;
+			LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+			if (myView == null) {
+				myView = inflater.inflate(resource, parent, false);
 			}
-			return fragment;
-		}
 
-		@Override
-		public int getCount() {
-			return 3;
+			TextView textTitle = (TextView) myView.findViewById(R.id.social_fragment_list_rowTitle);
+			TextView textDescription = (TextView) myView.findViewById(R.id.social_fragment_list_rowDescription);
+			
+			ListItem myItem = getItem(position);
+			textTitle.setText(myItem.title);
+			textDescription.setText(myItem.description);
+
+			return myView;
 		}
+		
+		@Override
+		public int getCount()	{
+			return items.size();
+		}
+		
+		@Override
+		public ListItem getItem(int position)	{
+			return items.get(position);
+		}
+		
+		@Override
+		public long getItemId(int position)	{
+			return getItem(position).id;
+		}
+		
+		public void addItem(ListItem item)	{
+			//items.add(new NavListItem(context.getResources().getDrawable( navArray[0] ), label));
+			items.add(item);
+		}
+		
 	}
 }
