a68099b70929b5225a459dba88b4439a9efe294d
afilipov93 <afilipov93@gmail.com>
2014-03-10 02:19:31 +0000

    Further work on Breathing Exercise and keyboard modification on Log Activity

--- src/com/team5/fragment/BreathExerciseFragment.java
+++ src/com/team5/fragment/BreathExerciseFragment.java
@@ -1,44 +1,56 @@
 package com.team5.fragment;
 
-import com.team5.exercise.AnimationCanvas;
 import com.team5.pat.HomeActivity;
 import com.team5.pat.R;
 
+import android.R.anim;
 import android.app.Fragment;
+import android.graphics.drawable.ScaleDrawable;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
+import android.webkit.WebView.FindListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class BreathExerciseFragment extends Fragment implements OnClickListener {
+public class BreathExerciseFragment extends Fragment implements OnClickListener
+{
 	// Layout components for breath exercise
-	private AnimationCanvas animCanvas;
 	private Button startButton;
 	private Button stopButton;
 	private View myView;
 	private HomeActivity myActivity;
-	private Handler mHandler;
-	private Runnable reDrawInhale;
-	private Runnable reDrawExhale;
-
-	// Constants that set frames per second, inhale and exhale values
-	private final int FPS = 50;
-	private final int INHALE_SECONDS = 1;
-	private final int EXHALE_SECONDS = 4;
-	private final int DRAW_DELAY = 1000 / FPS;
+	private ImageView image;
+	private Animation expand;
+	private int countDownSeconds;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+			Bundle savedInstanceState)
+	{
 		myView = inflater.inflate(R.layout.breath_exercise_layout, container,
 				false);
 		myActivity = (HomeActivity) getActivity();
 		myActivity.setTitle("Breathing");
 
 		initialiseComponents();
+
 		startButton.setOnClickListener(this);
 		stopButton.setOnClickListener(this);
 
@@ -46,53 +58,54 @@ public class BreathExerciseFragment extends Fragment implements OnClickListener 
 	}
 
 	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
+	public void onClick(View v)
+	{
+		switch (v.getId())
+		{
 		case R.id.startButton:
-			mHandler.post(reDrawInhale);
-			mHandler.post(reDrawExhale);
+
+			image.startAnimation(expand);
+			timer(countDownSeconds);
+
 			break;
 		case R.id.stopButton:
-			mHandler.removeCallbacks(reDrawInhale);
-			mHandler.removeCallbacks(reDrawExhale);
+
 			break;
 		}
 	}
 
-	private void initialiseComponents() {
+	private void initialiseComponents()
+	{
+
+		image = (ImageView) myView.findViewById(R.id.asd_id);
+
+		expand = AnimationUtils.loadAnimation(myActivity,
+				R.animator.breath_exercise_anim);
+
 		startButton = (Button) myView.findViewById(R.id.startButton);
 		stopButton = (Button) myView.findViewById(R.id.stopButton);
-		animCanvas = (AnimationCanvas) myView.findViewById(R.id.animationCanvas);
-		mHandler = new Handler();
-		reDrawInhale = new Runnable() {
-			public void run() {
-				if (animCanvas.backgroundCircleRadius <= animCanvas.dynamicCircleRadius) {
-					mHandler.removeCallbacks(this);
-				} else {
-					animCanvas.dynamicCircleRadius = animCanvas.dynamicCircleRadius
-							+ (animCanvas.backgroundCircleRadius - animCanvas.centerCircleRadius)
-							/ (INHALE_SECONDS * FPS);
-					animCanvas.invalidate();
 
-					mHandler.postDelayed(this, DRAW_DELAY);
-				}
+	}
+
+	private void timer(long remainingTime)
+	{
+		long secondsTick = 1000;
+		countDownSeconds = Integer.getInteger(((EditText) myView
+				.findViewById(R.id.secondsInput)).getText().toString(), 0);
+
+		new CountDownTimer(remainingTime, secondsTick) {
+
+			public void onTick(long millisUntilFinished)
+			{
+				countDownSeconds--;
+				((EditText) myView.findViewById(R.id.secondsInput))
+						.setText(String.valueOf(countDownSeconds));
 			}
 
-		};
-		reDrawExhale = new Runnable() {
-			public void run() {
-				if (animCanvas.centerCircleRadius >= animCanvas.dynamicCircleRadius) {
-					mHandler.removeCallbacks(this);
-				} else {
-					animCanvas.dynamicCircleRadius = animCanvas.dynamicCircleRadius
-							- (animCanvas.backgroundCircleRadius - animCanvas.centerCircleRadius)
-							/ (EXHALE_SECONDS * FPS);
+			public void onFinish()
+			{
 
-					animCanvas.invalidate();
-
-					mHandler.postDelayed(this, DRAW_DELAY);
-				}
 			}
-		};
+		}.start();
 	}
 }
