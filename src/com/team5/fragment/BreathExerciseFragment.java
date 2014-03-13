1574d40e4cc1c6e9097eefe7baf7cae918cea567
afilipov93 <afilipov93@gmail.com>
2014-03-13 02:03:18 +0000

    Breath Exercise ready for testing and debugging

--- src/com/team5/fragment/BreathExerciseFragment.java
+++ src/com/team5/fragment/BreathExerciseFragment.java
@@ -1,22 +1,26 @@
 package com.team5.fragment;
 
+import com.team5.graph.Line;
 import com.team5.pat.HomeActivity;
 import com.team5.pat.R;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
+import android.view.animation.LinearInterpolator;
 import android.view.animation.ScaleAnimation;
 import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 public class BreathExerciseFragment extends Fragment implements OnClickListener
 {
@@ -26,11 +30,12 @@ public class BreathExerciseFragment extends Fragment implements OnClickListener
 	private View myView;
 	private HomeActivity myActivity;
 	private ImageView image;
-	private Animation expand;
 	private int countDownSeconds;
 	private EditText secondsEditTextView;
 	private CountDownTimer timer;
-	private boolean timerToggled;
+	private boolean timerToggled = false;
+	private Animation expand;
+	private boolean stopButtonToggled = false;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -43,9 +48,6 @@ public class BreathExerciseFragment extends Fragment implements OnClickListener
 
 		initialiseComponents();
 
-		startButton.setOnClickListener(this);
-		stopButton.setOnClickListener(this);
-
 		return myView;
 	}
 
@@ -55,73 +57,131 @@ public class BreathExerciseFragment extends Fragment implements OnClickListener
 		switch (v.getId())
 		{
 		case R.id.startButton:
-			
-			if (!timerToggled)
+			//get the set time for the animation to run
+			countDownSeconds = Integer.parseInt(secondsEditTextView.getText()
+					.toString());
+			// if the timer is not started and the seconds differ from 0 start the animation
+			if (!timerToggled && countDownSeconds != 0)
 			{
-				
+
 				image.startAnimation(expand);
 				timer(countDownSeconds);
+				timerToggled = true;
+
 			}
 
 			break;
 		case R.id.stopButton:
-			
-			if(timerToggled)
+
+			if (timerToggled)
 			{
-			image.clearAnimation();
-			timer.cancel();
-			timerToggled = false;
+				// when the stop button is pressed - stop the timer, indicate
+				// that the stop button is pressed and stop the animation, then
+				// indicate the timer has been stopped
+				timer.cancel();
+				stopButtonToggled = true;
+				image.clearAnimation();
+				timerToggled = false;
 			}
+			break;
+
+		default:
+
 			break;
 		}
 	}
 
 	private void initialiseComponents()
 	{
+		// the seconds input field
 		secondsEditTextView = (EditText) myView.findViewById(R.id.secondsInput);
+		// the image that is scaled
 		image = (ImageView) myView.findViewById(R.id.dynamic_circle);
+		// the integer taken from the seconds input field
 		countDownSeconds = Integer.parseInt(secondsEditTextView.getText()
 				.toString());
-		expand = AnimationUtils.loadAnimation(myActivity,
+		// the animation that uses XML
+		expand = AnimationUtils.loadAnimation(
+				myActivity.getApplicationContext(),
 				R.animator.breath_exercise_anim);
 
+		// start and stop buttons and their listeners
 		startButton = (Button) myView.findViewById(R.id.startButton);
 		stopButton = (Button) myView.findViewById(R.id.stopButton);
-		
+		startButton.setOnClickListener(this);
+		stopButton.setOnClickListener(this);
+
+
+		// the animation listener to help with the repeat count
+		expand.setAnimationListener(new AnimationListener() {
+
+			@Override
+			public void onAnimationStart(Animation animation)
+			{
+			}
+
+			@Override
+			public void onAnimationRepeat(Animation animation)
+			{
+			}
+
+			@Override
+			public void onAnimationEnd(Animation animation)
+			{
+				// when the animation ends, restart it
+				image.startAnimation(expand);
+				// if the timer has finished and stop button is automatically
+				// clicked, stop the animation entirely
+				if (stopButtonToggled)
+				{
+					stopButtonToggled = false;
+					image.clearAnimation();
+					timerToggled = false;
+				}
+			}
+		});
 
 	}
-	
-	private void animation()
-	{
-		AnimationSet as = new AnimationSet(false); 
-		Animation expand;
-		Animation shrink;
-		int fromDuration;
-		int toDuration;
-		
-		expand = new ScaleAnimation(1.0f, 2.5f, 1.0f, 2.5f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-		expand.setDuration(2000);
-		shrink= new ScaleAnimation(1.0f, 2.5f, 1.0f, 2.5f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-	}
-	
+
+	// the timer
 	private void timer(long remainingTime)
 	{
 		long secondsTick = 1000;
 		timerToggled = true;
 
-		timer = new CountDownTimer(remainingTime * secondsTick, secondsTick) {
+		timer = new CountDownTimer((remainingTime + 1) * secondsTick,
+				secondsTick) {
 
 			public void onTick(long millisUntilFinished)
 			{
-				countDownSeconds--;
-				secondsEditTextView.setText("" + countDownSeconds);
+
+				if (millisUntilFinished < 2001)
+				{
+					// imitates the stop button click event, stops the timer and
+					// clears the animation
+					countDownSeconds--;
+					secondsEditTextView.setText("" + countDownSeconds);
+
+					stopButtonToggled = true;
+					timerToggled = false;
+					image.clearAnimation();
+					timer.cancel();
+				}
+
+				else
+				{
+					// updates the seconds left field and the progress bar on
+					// each
+					// tick and
+					countDownSeconds--;
+					secondsEditTextView.setText("" + countDownSeconds);
+				}
+
 			}
 
 			public void onFinish()
 			{
-				image.clearAnimation();
-				timer.cancel();
-				timerToggled = false;
+
 			}
 		}.start();
 	}
